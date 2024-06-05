c8195ec695f8e7724c9d500d5894b1317202fa1c
sidney <shogo12000@hotmail.com>
2024-06-05 00:57:28 -0700

    feat: [P2-100] create basic Pages

---
+++ src/pages/Rewards.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Rewards = () => {
+    return (
+        <main>
+            <h2>Rewards</h2>
+        </main>
+    );
+};
+
+export default Rewards;
---
+++ src/pages/Survey.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Survey = () => {
+    return (
+        <main>
+            <h2>Survey</h2>
+        </main>
+    );
+};
+
+export default Survey;
