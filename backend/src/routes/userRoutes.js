7d0f4f89f3285c6d603add4c9ba5df43ccbfca46
sidney <shogo12000@hotmail.com>
2024-06-08 22:13:15 -0700

    chore: [P2-118] mergedevelop

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const User = require("../models/UserModel");
 
-const router = express.Router();
+// const router = express.Router();
+const router = express();
+router.use(express.json());
 
 // Create a new user
 router.post("v1/user/register", async (req, res) => {
@@ -24,6 +26,11 @@ router.get("v1/users", async (req, res) => {
     } catch (error) {
         res.status(500).send(error.message);
     }
+});
+
+// Get all users
+router.get("v1/teste", async (req, res) => {
+    res.send("Hello World funcionando");
 });
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,21 @@ import { useState, useEffect } from "react";
 
 function App() {
     const [message, setMessage] = useState([]);
-
+    const [username, setUsername] = useState("admin");
+    const [password, setPassword] = useState("secret");
     const [showData, setShowData] = useState([]);
     const fetchSurvey = async () => {
+        const headers = new Headers();
+        //headers.set('Authorization', 'Basic ' + btoa(process.env.REACT_APP_USERNAME+ ':' + process.env.REACT_APP_PASSWORD));
+        headers.set(
+            "Authorization",
+            "Basic " + btoa(username + ":" + password)
+        );
+
         try {
-            const response = await fetch(process.env.REACT_APP_API_URL_SURVEY);
+            const response = await fetch(process.env.REACT_APP_API_URL_SURVEY, {
+                headers,
+            });
             if (response.ok) {
                 const data = await response.json();
                 console.log(data);
@@ -29,6 +39,11 @@ function App() {
 
     return (
         <div className="App">
+            {showData.length > 0 ? (
+                showData.map((e, index) => <p key={index}>{e.surveyID}</p>)
+            ) : (
+                <p>No messages to display</p>
+            )}
             {showData.length > 0 ? (
                 showData.map((e, index) => <p key={index}>{e.surveyID}</p>)
             ) : (
