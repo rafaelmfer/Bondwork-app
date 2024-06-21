db0e59e17303075f1adde498605c1626621a1411
sidney <shogo12000@hotmail.com>
2024-06-21 11:27:39 -0700

    feat: [P2-171] Create Summary Components

--- frontend/src/pages/SurveyMain.jsx
+++ frontend/src/pages/SurveyMain.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import SurveyTable from "../components/SurveyTable";
 import Chart from "../components/Charts/Chart";
+import { Summary } from "../components/Summary/Summary";
 
 const SurveyMain = () => {
     return (
         <main className="ml-menuMargin mt-24">
+            <Summary />
+
             <Chart />
 
             <SurveyTable />
