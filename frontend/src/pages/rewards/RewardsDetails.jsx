2e504f6d47f108b35df53399c8e43b9f56c53359
Rafael de Mello Ferreira <rafaelmfer@hotmail.com>
2024-06-25 16:29:12 -0700

    refactor: [P2-211] organize files and folders name (#37)

---
+++ frontend/src/pages/rewards/RewardsDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RewardsDetails = () => {
+    return (
+        <main>
+            <h2>Rewards</h2>
+        </main>
+    );
+};
+
+export default RewardsDetails;
---
+++ frontend/src/pages/rewards/RewardsManagement.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RewardsManagement = () => {
+    return (
+        <main>
+            <h2>Rewards</h2>
+        </main>
+    );
+};
+
+export default RewardsManagement;
