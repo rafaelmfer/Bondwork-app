2ff983d430a418fde08a915479beeccba356fe7a
Rafael de Mello Ferreira <rafaelmfer@hotmail.com>
2024-06-25 13:39:37 -0700

    refactor: [P2-208] divide the charts component and create more variants (#36)

--- frontend/src/pages/survey/SurveyMain.jsx
+++ frontend/src/pages/survey/SurveyMain.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import SurveyTable from "../../components/SurveyTable";
-import Chart from "../../components/Charts/Chart";
-import { Summary } from "../../components/Summary/Summary";
+import Summary from "../../components/Summary/Summary";
+import ChartArea from "../../components/Charts/ChartArea";
+import ChartDonut from "../../components/Charts/ChartDonut";
+import ChartLine from "../../components/Charts/ChartLine";
 
 const SurveyMain = () => {
     return (
         <main className="ml-menuMargin mt-24 bg-white">
             <Summary />
-            <Chart />
+            <div className="grid grid-cols-3 items-center gap-5 p-4" id="chart">
+                <ChartDonut />
+                <ChartArea />
+                <ChartLine />
+            </div>
 
             <SurveyTable rowsNumber="5" />
         </main>
