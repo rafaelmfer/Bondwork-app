3a5d65ee9f2dcebe8736778fe75e10be0863d90f
Rafael de Mello Ferreira <rafaelmfer@hotmail.com>
2024-06-28 08:41:41 -0700

    fix: [P2-221] update sidebar and top-user-bar component (#46)
    * fix: [P2-221] update sidebar and top-user-bar component
    
    * fix: [P2-222] change height of the charts to fit better in the pages (#47)

--- frontend/src/components/top-user-bar/TopUserBar.jsx
+++ frontend/src/components/top-user-bar/TopUserBar.jsx
@@ -1,12 +1,10 @@
 import styles from "./TopUserBar.module.css";
-import person from "../../assets/images/person.svg";
-import menu from "../../assets/images/menu.svg";
 import NotificationBadge from "../NotificationBadge";
 
 const TopUserBar = ({ titleScreen }) => {
     return (
         <div className={styles.topBar}>
-            <div>{titleScreen}</div>
+            <div className="text-h2">{titleScreen}</div>
             <div className={styles.right_side}>
                 {/* <img src={iconBell} alt="icon" className={styles.logo} /> */}
                 <NotificationBadge />
