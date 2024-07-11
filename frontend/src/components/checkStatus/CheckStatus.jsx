5c06f100a8ec8e592ba715db43ee7e2afaf8e7c0
cecilia314 <155802930+cecilia314@users.noreply.github.com>
2024-07-11 01:29:26 -0700

    feat: [P2-327] Add tables in Rewards  and recognition (#87)
    Co-authored-by: Rafael de Mello Ferreira <rafaelmfer@hotmail.com>

--- frontend/src/components/checkStatus/CheckStatus.jsx
+++ frontend/src/components/checkStatus/CheckStatus.jsx
@@ -1,27 +1,42 @@
 import { ReactComponent as RedBullet } from "../../assets/images/dot-Red.svg";
 import { ReactComponent as GreenBullet } from "../../assets/images/dot-Green.svg";
 import { ReactComponent as SilverBullet } from "../../assets/images/dot-Gray.svg";
+import { ReactComponent as OrangeBullet } from "../../assets/images/dot-Yellow.svg";
+import { ReactComponent as BlueBullet } from "../../assets/images/dot-Blue.svg";
+export function CheckStatus({ status }) {
+    let bulletComponent, statusClass;
 
-export function CheckStatus(props) {
+    switch (status) {
+        case "Pending":
+        case "Ongoing":
+            bulletComponent = <BlueBullet />;
+            statusClass = "text-info-300";
+            break;
+        case "Approved":
+        case "Finished":
+            bulletComponent = <GreenBullet />;
+            statusClass = "text-success-300";
+            break;
+        case "Rejected":
+            bulletComponent = <RedBullet />;
+            statusClass = "text-alert-300";
+            break;
+        case "Draft":
+            bulletComponent = <SilverBullet />;
+            statusClass = "text-neutrals-gray300";
+            break;
+        case "Upcoming":
+            bulletComponent = <OrangeBullet />;
+            statusClass = "text-warning-300";
+            break;
+        default:
+            bulletComponent = <SilverBullet />;
+            statusClass = "text-neutrals-gray300";
+    }
+
     return (
-        <p className=" items-center gap-3 flex text-p">
-            {props.status === "Pending" ? (
-                <>
-                    <RedBullet />{" "}
-                    <span className="text-alert-300">Pending</span>
-                </>
-            ) : props.status === "Approved" ? (
-                <>
-                    <GreenBullet />{" "}
-                    <span className="text-success-300">Approved</span>
-                </>
-            ) : (
-                <>
-                    {" "}
-                    <SilverBullet />{" "}
-                    <span className="text-neutrals-gray300">Reject</span>
-                </>
-            )}
+        <p className=" items-center gap-3 flex text-p text-sans">
+            {bulletComponent} <span className={statusClass}>{status}</span>
         </p>
     );
 }
